1efbba1da7cd0c92d2cafe8e0a0ec6a8574e198c
Victoria Hale <vic.hale74@gmail.com>
2019-10-10 22:37:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(array)
   
 end
 
-def find_the_cheese(array)# code an argument here
+def find_the_cheese(array)
   array.find do |cheese|
   cheese == "cheddar" || cheese == "gouda" || cheese== "camambert"
   end
